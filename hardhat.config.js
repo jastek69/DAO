05b5e9cd037545a53590aa5a9503feb2ab767b7d
John <jastek@SEBEK.localdomain>
2023-07-12 01:01:45 -0700

    Sepolia deploy: WIP

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
+const { task } = require("hardhat/config");
+
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
+// Hardhat Tasks
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+})
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    localhost: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(','),
+    }
+  },
 };
